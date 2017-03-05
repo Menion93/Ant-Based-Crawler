fabf293bc6f873bec2b8c2dee7ed66eb91cfe436
Menion93 <andrea.salvoni93@gmail.com>
2017-03-05 17:37:56 +0100

    Added main class

---
+++ src/AntBasedCrawler.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Andrea on 05/03/2017.
+ */
+public class AntBasedCrawler {
+
+
+    public static void main(String[] args)
+    {
+           System.out.println("Hello world");
+    }
+
+
+}
