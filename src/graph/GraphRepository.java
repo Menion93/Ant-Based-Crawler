21840f98e6860b285fac866f392564f4a4a212da
Menion93 <andrea.salvoni93@gmail.com>
2017-03-06 22:30:48 +0100

    a Repository for the db

---
+++ src/graph/GraphRepository.java
@@ -0,0 +1,13 @@
+package graph;
+
+/**
+ * Created by Andrea on 06/03/2017.
+ */
+public class GraphRepository
+{
+
+    public NodePage getNodePageRoot()
+    {
+        return new NodePage();
+    }
+}
---
+++ src/graph/NodePage.java
@@ -0,0 +1,11 @@
+package graph;
+
+/**
+ * Created by Andrea on 06/03/2017.
+ */
+public class NodePage
+{
+    float id;
+    String content;
+
+}
