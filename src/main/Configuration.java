5576bdcfd88c13760d4f90da0d469208794ee2f5
Menion93 <andrea.salvoni93@gmail.com>
2017-03-13 19:43:00 +0100

    now main and conf are under main package

---
+++ src/main/Configuration.java
@@ -0,0 +1,45 @@
+package main; /**
+ * Created by Andrea on 05/03/2017.
+ *
+ * This class define the behaviour of the ant based crawler
+ *
+ */
+import scorer.*;
+
+public class Configuration
+{
+    private int numberOfAnts = 100;
+    private int maxPagesToVisit = 1000;
+    private int maxNumberOfIteration = 100;
+    private double trailUpdateCoefficient = 0.1;
+    private double randomInitValue = 0.5;
+
+    private String dictionaryScoring = "DictionaryScorer";
+    private String classifierScoring = "ClassifierScorer";
+
+    private String query = "University of Rome 3";
+
+    public int getNumberOfAnts() {
+        return numberOfAnts;
+    }
+
+    public int getMaxPagesToVisit() {
+        return maxPagesToVisit;
+    }
+
+    public int getMaxNumberOfIteration() {
+        return maxNumberOfIteration;
+    }
+
+    public String getScoringMethod() {
+        return dictionaryScoring;
+    }
+
+    public double getTrailUpdateCoefficient() {return trailUpdateCoefficient; }
+
+    public double getRandomInitValue() { return randomInitValue; }
+
+    public String getQuery() {
+        return query;
+    }
+}
