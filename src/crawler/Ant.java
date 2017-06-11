8f167b86b6233b348bc23b51e6eb4afa1b982c7c
Menion93 <andrea.salvoni93@gmail.com>
2017-06-11 17:27:11 +0200

    Refactor of the Ants algorithm, now it is easier to read, made also improvements about the caching of the pages

--- src/crawler/Ant.java
+++ src/crawler/Ant.java
@@ -1,9 +1,117 @@
 package crawler;
 
+import graph.GraphRepository;
+import graph.NodePage;
+import scorer.Scorer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Andrea on 07/03/2017.
  */
 public class Ant
 {
-    // Ant class : to do -> refactor
+
+    private List<Edge> path;
+    private Scorer scorer;
+    private Random randomGenerator;
+    private double randomInitValue;
+
+    HashMap<Edge, Double > edge2trail;
+
+
+    public Ant(Scorer scorer, HashMap<Edge, Double > edge2trail, double randomInitValue, int seed){
+        this.path = new ArrayList<>();
+        this.scorer = scorer;
+        this.edge2trail = edge2trail;
+        this.randomInitValue = randomInitValue;
+        randomGenerator = new Random(seed);
+
+    }
+
+
+    public void AntCycle(HashMap<String,Double> id2score, GraphRepository graphRepo, int numberOfStep) throws IOException {
+
+
+        // Start the iteration of the i-th ant
+        NodePage currentNode = graphRepo.getNodePageRoot();
+
+        if(!id2score.containsKey(currentNode.getId()))
+        {
+            id2score.put(currentNode.getId(), scorer.predictScore(currentNode));
+            currentNode.freeContentMemory();
+        }
+
+        for(int j=0; j<numberOfStep;j++)
+        {
+            List<NodePage> frontier = graphRepo.expandNode(currentNode);
+
+            NodePage successorNode = selectNode(currentNode, frontier);
+
+            if(!id2score.containsKey(successorNode.getId()))
+            {
+                id2score.put(successorNode.getId(), scorer.predictScore(successorNode));
+                successorNode.freeContentMemory();
+            }
+
+            path.add(new Edge(currentNode.getId(), successorNode.getId()));
+
+            currentNode = successorNode;
+        }
+
+    }
+
+    // To do: select a node with probability according to the trail, or randomValue
+    // if it is the last iteration
+    private NodePage selectNode(NodePage currentNode, List<NodePage> frontier)
+    {
+        double[] trailsProba = new double[frontier.size()];
+        double sumTrails = 0;
+
+        for(int i=0; i<frontier.size(); i++)
+        {
+            Edge e = new Edge(currentNode.getId(), frontier.get(i).getId());
+            if(edge2trail.containsKey(e))
+            {
+                trailsProba[i] = edge2trail.get(e);
+            }
+            else
+            {
+                edge2trail.put(e, randomInitValue);
+                trailsProba[i] = randomInitValue;
+            }
+
+            sumTrails += trailsProba[i];
+        }
+
+        // Normalize probability
+        for(int i=0; i<trailsProba.length; i++)
+        {
+            trailsProba[i] /= sumTrails;
+        }
+
+
+        // Chose random a successor between those probability
+        double nodeChoice = randomGenerator.nextDouble();
+        double currentProba = 0;
+
+        for(int i=0; i<trailsProba.length; i++)
+        {
+            currentProba += trailsProba[i];
+
+            if(nodeChoice < currentProba)
+                return frontier.get(i);
+        }
+
+        System.out.println("ERROR in the selection of the successor");
+        return null;
+    }
+
+    public List<Edge> getPath() {
+        return path;
+    }
 }
--- src/crawler/AntBasedCrawler.java
+++ src/crawler/AntBasedCrawler.java
@@ -5,12 +5,12 @@ package crawler;
  */
 
 import graph.GraphRepository;
-import graph.NodePage;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Random;
-import scorer.Scorer;
+import java.util.List;
+import scorer.ScorerFactory;
 
 public class AntBasedCrawler
 {
@@ -19,56 +19,60 @@ public class AntBasedCrawler
     private int maxPagesToVisit;
     private int maxNumberOfIteration ;
     private int visitedPages;
-    private int numberOfStep;
     private double trailUpdateCoefficient;
-    private double randomInitValue;
 
-    private Scorer scorer;
     private GraphRepository graphRepo;
 
     // This map represent the id of the pages and its scoreClassifier.
     // It is a cache of the scores of the single pages
-    private HashMap<Double, Double> id2score;
+    private HashMap<String, Double> id2score;
 
-    // Map representation of the arc and its associated trail value
-    private HashMap<Arc, Double > arc2trail;
+    // Map representation of the edge and its associated trail value
+    private HashMap<Edge, Double > edge2trail;
 
-    private Random randomGenerator;
+    private List<Ant> ants;
+
 
     public AntBasedCrawler(int numberOfAnts, int maxNumberOfIteration, int maxPagesToVisit,
-                           double trailUpdateCoefficient, double randomInitValue, Scorer scorer)
+                           double trailUpdateCoefficient, double randomInitValue, ScorerFactory scFactory,
+                           GraphRepository graphRepo)
     {
         this.numberOfAnts = numberOfAnts;
         this.maxNumberOfIteration = maxNumberOfIteration;
         this.maxPagesToVisit = maxPagesToVisit;
-        this.scorer = scorer;
         this.trailUpdateCoefficient = trailUpdateCoefficient;
-        this.randomInitValue = randomInitValue;
 
         id2score = new HashMap<>();
-        arc2trail = new HashMap<>();
+        edge2trail = new HashMap<>();
 
-        graphRepo = new GraphRepository();
+        this.graphRepo = graphRepo;
 
-        randomGenerator = new Random(6337);
+        ants = new ArrayList<>();
+
+        // Create all the Ants
+        for(int i=0; i<numberOfAnts; i++)
+            ants.add(new Ant(scFactory.getScorer(), edge2trail, randomInitValue,  6337 + i));
+
     }
 
-    public double[] FetchPagesId()
-    {
+    public List<String> FetchPagesId() throws IOException {
+        List<String> crawledPages = new ArrayList<>();
+
         visitedPages = 0;
-        numberOfStep = 0;
+        int numberOfStep = 0;
 
         System.out.println("Starting the crawling...");
         double startTime = System.currentTimeMillis() * 1000;
 
 
-        while (haltingCriterion())
+        while (haltingCriterion(numberOfStep))
         {
             // Get the visited pages by the ants
-            ArrayList<ArrayList<Arc>> paths = DoAntsCycle();
+            for(Ant ant : ants)
+                ant.AntCycle(id2score, graphRepo, numberOfStep);
 
             // Update the trails
-            UpdateTrails(paths);
+            UpdateTrails(ants, numberOfStep);
 
             // Increment the step of the ants can take by one
             numberOfStep++;
@@ -80,131 +84,48 @@ public class AntBasedCrawler
         System.out.format("In mean the crawling spends about %f seconds per iteration\n", lastedTime/(numberOfStep-1));
 
         // To Do: return the pages crawled, maybe a ranking?
-        return new double[]{1,2,3};
+        return crawledPages;
     }
 
-    // Need to refactorize the code with class Ant
-    private ArrayList<ArrayList<Arc>> DoAntsCycle()
-    {
-        // A list of list representing all the paths taken by all the ants
-        ArrayList<ArrayList<Arc>> path = new ArrayList<>();
 
-        // For all the ants
-        for(int i=0; i<numberOfAnts; i++)
-        {
-            ArrayList<Arc> singlePath = new ArrayList<>();
-
-            // Start the iteration of the i-th ant
-            NodePage currentNode = graphRepo.getNodePageRoot();
-
-            if(numberOfStep == 1 && i == 0)
-            {
-                id2score.put(currentNode.getId(), scorer.predictScore(currentNode));
-                currentNode.freeContentMemory();
-            }
-
-            for(int j=0; j<numberOfStep;j++)
-            {
-                NodePage[] frontier = graphRepo.expandeNode(currentNode);
-
-                NodePage successorNode = selectNode(currentNode, frontier);
-
-                if(!id2score.containsKey(successorNode.getId()))
-                {
-                    id2score.put(successorNode.getId(), scorer.predictScore(successorNode));
-                    successorNode.freeContentMemory();
-                }
-
-                singlePath.add(new Arc(currentNode.getId(), successorNode.getId()));
-
-                currentNode = successorNode;
-            }
-
-            path.add(singlePath);
-        }
-
-        return path;
-    }
 
     // Update the trails after an ant epoch
-    private void UpdateTrails(ArrayList<ArrayList<Arc>> paths)
+    private void UpdateTrails(List<Ant> ants, int numberOfStep)
     {
         // For all ants
-        for(ArrayList<Arc> singlePath : paths)
+        for(Ant ant : ants)
         {
             // For all pages the ant walked on
             // Compute the trail score for the path
-            // and update the arc2trail list
+            // and update the edge2trail list
 
-            double pathScore = id2score.get(singlePath.get(0).getFrom());
+            List<Edge> path = ant.getPath();
+
+            double pathScore = id2score.get(path.get(0).getFrom());
 
             for(int j = 0; j<numberOfStep; j++)
             {
-                pathScore += id2score.get(singlePath.get(j).getTo());
+                pathScore += id2score.get(path.get(j).getTo());
             }
 
             // Normalization
             pathScore /= (numberOfStep+1);
 
             // Update the trail score of the visited page by this ant
-            for(Arc e : singlePath)
+            for(Edge e : path)
             {
-                arc2trail.put(e, trailUpdateCoefficient*arc2trail.get(e) + pathScore);
+                edge2trail.put(e, trailUpdateCoefficient* edge2trail.get(e) + pathScore);
             }
         }
 
     }
 
     // It counts the same pages multiple times, need to find a better halting criterion
-    private boolean haltingCriterion()
+    private boolean haltingCriterion(int numberOfStep)
     {
         return (visitedPages + numberOfAnts * numberOfStep) <= maxPagesToVisit;
     }
 
-    // To do: select a node with probability according to the trail, or randomValue
-    // if it is the last iteration
-    private NodePage selectNode(NodePage currentNode, NodePage[] frontier)
-    {
-        double[] trailsProba = new double[frontier.length];
-        double sumTrails = 0;
 
-        for(int i=0; i<frontier.length; i++)
-        {
-            Arc e = new Arc(currentNode.getId(), frontier[i].getId());
-            if(arc2trail.containsKey(e))
-            {
-                trailsProba[i] = arc2trail.get(e);
-            }
-            else
-            {
-                arc2trail.put(e, randomInitValue);
-                trailsProba[i] = randomInitValue;
-            }
-
-            sumTrails += trailsProba[i];
-        }
-
-        // Normalize probability
-        for(int i=0; i<trailsProba.length; i++)
-        {
-            trailsProba[i] /= sumTrails;
-        }
-
-
-        // Chose random a successor between those probability
-        double nodeChoice = randomGenerator.nextDouble();
-        double currentProba = 0;
-
-        for(int i=0; i<trailsProba.length; i++)
-        {
-            currentProba += trailsProba[i];
-
-            if(nodeChoice < currentProba)
-                return frontier[i];
-        }
-
-        System.out.println("ERROR in the selection of the successor");
-        return null;
-    }
 
 }
