176fd62cbfcbba4412b6c87eafe6cdc9b32e1fd7
Menion93 <andrea.salvoni93@gmail.com>
2017-06-16 20:13:04 +0200

    renamed the scorer to a more suitable name

---
+++ src/scorer/HeaderScorer.java
@@ -0,0 +1,80 @@
+package scorer;
+
+import graph.NodePage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * Created by Andrea on 05/03/2017.
+ */
+public class ClassifierScorer extends Scorer
+{
+
+    public ClassifierScorer(String query)
+    {
+        super(query);
+    }
+
+    // It checks the hit on the h1, meta descriptions and the title, and calculate a score
+    public double predictScore(NodePage nodePage) {
+
+        double resultScore = 0;
+
+        String content = nodePage.getContent();
+        Document doc = Jsoup.parse(content);
+
+        // Get the title and compute the hit
+        String title = doc.title();
+        resultScore += countPercentageHit(title);
+
+
+        Elements meta = doc.select("meta[name=description]");
+
+        // Get the description if the page has one and compute the hit
+        String description = null;
+
+        if(meta != null){
+            description = meta.attr("content");
+            resultScore += countPercentageHit(description);
+        }
+
+        // Get the h1 if the page has one and compute the hit
+        Elements h1 = doc.select("h1");
+
+        double currentPercentage = 0;
+
+        for(Element e : h1){
+            double temp = countPercentageHit(e.text());
+
+            if(currentPercentage < temp)
+                currentPercentage = temp;
+        }
+
+        resultScore += currentPercentage;
+
+        return resultScore;
+    }
+
+    public double countPercentageHit(String text){
+
+        double hits = 0;
+
+        String whiteSpace = " ";
+        String[] words = query.split(whiteSpace);
+        String[] splittedText = text.split(whiteSpace);
+
+        for(String word : words){
+            for(String textWord : splittedText){
+                if(word.equals(textWord)){
+                    hits++;
+                    break;
+                }
+            }
+        }
+
+        return hits/words.length;
+    }
+
+}
